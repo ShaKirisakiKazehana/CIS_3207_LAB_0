ab38d241e2b1ca015efc1d3a347f7a6067e50d5d
Dongjue Xie <x5176518@gmail.com>
2022-08-25 21:24:30 -0400

    Main

--- lab0.c
+++ lab0.c
@@ -17,26 +17,26 @@ main(int argc, char *argv[])
 {
 	DIR *folder;
 	struct dirent *entry;
-
 	char *path = argv[1];
-
 	int files = 0;
+    
 	if (argv[1] != NULL)
         	folder = opendir(path);
     	else
         	folder = opendir(".");
     	if (folder == NULL)
     	{
-        	perror("Unable to read directory");
+        	perror("tuls");
+            puts("ERROR: EXIT_FAILURE");
         	return(1);
 	}
-
-    	while ( (entry = readdir(folder)) )
-    	{
-        	files++;
-        	printf("File %2d: %s\n", files,
-                entry->d_name);
-    	}
+    if(path != NULL)
+        printf("Loading %s\n", path);
+    while ( (entry = readdir(folder)) )
+    {
+        files++;
+        printf("File %2d: %s\n", files, entry->d_name);
+    }
 
 	closedir(folder);
 	return EXIT_SUCCESS;
