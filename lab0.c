39a1641eba2ed422e2ab8d1cd74f4b48e72079a6
Dongjue Xie <x5176518@gmail.com>
2022-08-25 21:00:20 -0400

    main

--- lab0.c
+++ lab0.c
@@ -2,7 +2,16 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
-
+/*
+ * Location: Temple University Computer Science
+ * Programmer: Dongjue Xie
+ * Class: CIS_3207
+ * Assignment: Lab 0
+ * Date: August 25, 2022
+ * Version: 0
+ * Description: A simple implementation of the
+ * Unix binary "ls"
+ */
 int
 main(int argc, char *argv[])
 {
