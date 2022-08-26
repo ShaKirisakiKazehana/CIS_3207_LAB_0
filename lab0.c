b2154a81d62b41dca33e11138b57c1d30b3221ee
Dongjue Xie <x5176518@gmail.com>
2022-08-25 22:06:34 -0400

    commit

--- lab0.c
+++ lab0.c
@@ -11,14 +11,17 @@
  * Version: 0
  * Description: A simple implementation of the
  * Unix binary "ls"
- */
+ */ 
+
+char* file_type(int);
+
 int
 main(int argc, char *argv[])
 {
 	DIR *folder;
 	struct dirent *entry;
 	char *path = argv[1];
-	int files = 0;
+	int items = 0;
     
 	if (argv[1] != NULL)
         	folder = opendir(path);
@@ -34,10 +37,23 @@ main(int argc, char *argv[])
         printf("Loading %s\n", path);
     while ( (entry = readdir(folder)) )
     {
-        files++;
-        printf("File %2d: %s\n", files, entry->d_name);
+        items++;
+        printf("item %2d  %5s: %s\n", items, 
+		file_type(entry->d_type), entry->d_name);
     }
 
 	closedir(folder);
 	return EXIT_SUCCESS;
+}
+
+/*
+ * A simple function that will return type information
+ * as a string
+ */
+char*
+file_type(int d_type)
+{
+	if(d_type == 4)
+		return "DIR";
+	return "FILE";
 }
